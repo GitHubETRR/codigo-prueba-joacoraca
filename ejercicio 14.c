d31ba84e6f58e7710f9983f33ff20079b859252f
joacoraca <racamatojoaquin@gmail.com>
2024-06-05 16:02:16 -0300

    ejercicio 14

--- ejercicio 14.c
+++ ejercicio 14.c
@@ -1,22 +1,18 @@
-//Construye un programa que permita ingresar los valores de 2 de los ángulos interiores de un triángulo, y se emita por pantalla el valor del restante.
 #include <stdio.h>
 
-int main(){
+int main() {
+    float angulo1, angulo2, angulo3;
 
-    printf("Ingresa el primer valor de tus angulos: \n");
-    float primerangulo;
-    scanf("%.2f", &primerangulo);
-    printf("Ingresa el valor del segundo angulo: \n");
-    float segundoangulo;
-    scanf("%.2f", &segundoangulo);
-    if (primerangulo>180 || segundoangulo>180)
-    {
-        printf("Un triangulo no puede tener un angulo de 180 o mayor");
-    }
-    else {
-        float tercerangulo;
-        tercerangulo=180-primerangulo-segundoangulo;
-        printf("\nEl tercer valor de tu angulo es: %.2f", tercerangulo);
-    }
+    printf("Ingrese el valor del primer ángulo (en grados): ");
+    scanf("%f", &angulo1);
+
+    printf("Ingrese el valor del segundo ángulo (en grados): ");
+    scanf("%f", &angulo2);
+
+    // Calculo el tercer ángulo
+    angulo3 = 180 - (angulo1 + angulo2);
+
+    printf("El valor del tercer ángulo es: %.2f grados\n", angulo3);
+
     return 0;
 }
