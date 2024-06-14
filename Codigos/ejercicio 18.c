49b4ecfdccb4f14acee44161b27a151e36a16d48
joacoraca <racamatojoaquin@gmail.com>
2024-06-14 09:04:05 -0300

    Update ejercicio 18.c

--- Codigos/ejercicio 18.c
+++ Codigos/ejercicio 18.c
@@ -5,9 +5,9 @@ int main() {
     printf("Bienvenido a la calculadora de area");
     const float PI = 3.14; 
     float radio;
-    float area = PI * radio * radio;
     printf("Ingresa el radio del circulo: ");
     scanf("%f", &radio);
+    float area = PI * radio * radio;
     printf("El area del circulo es: %f\n", area);
     return 0;
 }
