0df4d9ffd839b43df3f9a11a9b8aa71517d34436
joacoraca <racamatojoaquin@gmail.com>
2024-07-02 10:35:40 -0300

    commit

--- Codigos/ejercicio 19.c
+++ Codigos/ejercicio 19.c
@@ -4,10 +4,11 @@
 
 int main (){
 
-    float cateto1, cateto2, hipotenusa=sqrt(pow(cateto1, 2)+pow(cateto2, 2));
+    float cateto1, cateto2, hipotenusa;
     printf("Introduce el valor del primer cateto: \n");
-    scanf("%f", cateto1);
+    scanf("%f", &cateto1);
     printf("Introduce el valor del segundo cateto: \n");
-    scanf("%f", cateto2);
-    printf("El valor de la hipotenusa es de %f", hipotenusa);
+    scanf("%f", &cateto2);
+    hipotenusa=sqrt(pow(cateto2, 2)+pow(cateto1, 2));
+    printf("El valor de la hipotenusa es de %.2f", hipotenusa);
 }
