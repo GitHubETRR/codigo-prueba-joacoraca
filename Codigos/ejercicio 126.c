cfcf07799b327bf3ba68a904f293d4c24828f796
joacoraca <racamatojoaquin@gmail.com>
2024-11-22 11:06:59 -0300

    Update ejercicio 126.c

--- Codigos/ejercicio 126.c
+++ Codigos/ejercicio 126.c
@@ -4,30 +4,30 @@ typedef struct {
     int dia;
     int mes;
     int anio;
-} Fecha;
+} fecha_t;
 
-int getDia(Fecha f) {
+int getDia(fecha_t f) {
     return f.dia;
 }
 
-int getMes(Fecha f) {
+int getMes(fecha_t f) {
     return f.mes;
 }
 
-int getAnio(Fecha f) {
+int getAnio(fecha_t f) {
     return f.anio;
 }
 
-int isBisiesto(Fecha f) {
+int siEsBisiesto(fecha_t f) {
     return (f.anio % 4 == 0 && f.anio % 100 != 0) || (f.anio % 400 == 0);
 }
 
 int main() {
-    Fecha hoy = {22, 11, 2024};
+    fecha_t hoy = {22, 11, 2024};
     printf("Día: %d\n", getDia(hoy));
     printf("Mes: %d\n", getMes(hoy));
     printf("Año: %d\n", getAnio(hoy));
-    if (isBisiesto(hoy)) {
+    if (siEsBisiesto(hoy)) {
         printf("El año %d es bisiesto.\n", getAnio(hoy));
     } else {
         printf("El año %d no es bisiesto.\n", getAnio(hoy));
